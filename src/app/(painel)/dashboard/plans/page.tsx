3807168fee223cc8c9e08c9a30d2af65aa241a84
Vitor Torquato de Sousa <torquatovitor68@gmail.com>
2025-03-25 15:30:23 +0100

    protecting pages profile and plan

--- src/app/(painel)/dashboard/plans/page.tsx
+++ src/app/(painel)/dashboard/plans/page.tsx
@@ -1,4 +1,12 @@
-export default function Plans() {
+import getSession from "@/lib/getSession";
+import {redirect} from 'next/navigation'
+
+export default async function Plans() {
+  const session = await getSession();
+      
+      if(!session){
+        redirect("/")
+      }
   return (
     <section>
         <h1>Planos</h1>
--- src/app/(painel)/dashboard/profile/page.tsx
+++ src/app/(painel)/dashboard/profile/page.tsx
@@ -1,4 +1,16 @@
-export default function Profile() {
+import getSession from "@/lib/getSession";
+import {redirect} from 'next/navigation'
+
+export default  async function Profile() {
+
+    const session = await getSession();
+    
+    if(!session){
+      redirect("/")
+    }
+
+    
+
   return (
     <section>
         <h1>Profile</h1>
